cf94ae80e06c66e3ce1b51e049d63153d510ddd5
Firestar311 <slaterandrew0398@gmail.com>
2022-04-28 03:17:57 -0500

    Added server specicic implementation for the ServerManager. This will be unused for a bit though

--- src/main/java/com/thenexusreborn/nexuscore/SpigotNexusAPI.java
+++ src/main/java/com/thenexusreborn/nexuscore/SpigotNexusAPI.java
@@ -4,6 +4,7 @@ import com.thenexusreborn.api.NexusAPI;
 import com.thenexusreborn.api.data.DataManager;
 import com.thenexusreborn.api.tags.TagManager;
 import com.thenexusreborn.nexuscore.player.*;
+import com.thenexusreborn.nexuscore.server.SpigotServerManager;
 import com.thenexusreborn.nexuscore.thread.SpigotThreadFactory;
 
 import java.sql.*;
@@ -13,7 +14,7 @@ public class SpigotNexusAPI extends NexusAPI {
     private NexusCore plugin;
     
     public SpigotNexusAPI(NexusCore plugin) {
-        super(plugin.getLogger(), new DataManager(), new TagManager(), new SpigotPlayerManager(plugin), new SpigotThreadFactory(plugin), new SpigotPlayerFactory());
+        super(plugin.getLogger(), new DataManager(), new TagManager(), new SpigotPlayerManager(plugin), new SpigotThreadFactory(plugin), new SpigotPlayerFactory(), new SpigotServerManager(plugin));
         this.plugin = plugin;
     }
     
---
+++ src/main/java/com/thenexusreborn/nexuscore/server/SpigotServerManager.java
@@ -0,0 +1,34 @@
+package com.thenexusreborn.nexuscore.server;
+
+import com.thenexusreborn.api.NexusAPI;
+import com.thenexusreborn.api.server.*;
+import com.thenexusreborn.nexuscore.NexusCore;
+import com.thenexusreborn.nexuscore.util.ServerProperties;
+import org.bukkit.Bukkit;
+
+public class SpigotServerManager extends ServerManager {
+    
+    private NexusCore plugin;
+    
+    public SpigotServerManager(NexusCore plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public void setupCurrentServer() {
+        int multicraftId = plugin.getConfig().getInt("serverinfo.multicraftid");
+        String ip = ServerProperties.getServerIp();
+        String name = plugin.getConfig().getString("serverinfo.name");
+        int port = ServerProperties.getServerPort();
+        int players = Bukkit.getOnlinePlayers().size();
+        int maxPlayers = Bukkit.getMaxPlayers();
+        int hiddenPlayers = 0;
+        String type = plugin.getConfig().getString("serverinfo.type");
+        String status = "loading";
+        String state = "none";
+        this.currentServer = new ServerInfo(multicraftId, ip, name, port, players, maxPlayers, hiddenPlayers, type, status, state);
+        if (multicraftId > 0) {
+            NexusAPI.getApi().getDataManager().pushServerInfoAsync(this.currentServer);
+        }
+    }
+}
