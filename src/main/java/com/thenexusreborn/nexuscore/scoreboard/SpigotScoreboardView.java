52098b534330ade0e6f0e38082b72f9a44b30cee
Firestar311 <slaterandrew0398@gmail.com>
2022-04-28 14:41:10 -0500

    Added some quality of life methods for creating teams in scoreboards

--- src/main/java/com/thenexusreborn/nexuscore/scoreboard/SpigotScoreboardView.java
+++ src/main/java/com/thenexusreborn/nexuscore/scoreboard/SpigotScoreboardView.java
@@ -1,7 +1,9 @@
 package com.thenexusreborn.nexuscore.scoreboard;
 
 import com.thenexusreborn.api.scoreboard.*;
+import com.thenexusreborn.api.scoreboard.wrapper.ITeam;
 import com.thenexusreborn.nexuscore.scoreboard.impl.SpigotObjective;
+import com.thenexusreborn.nexuscore.util.MCUtils;
 import org.bukkit.scoreboard.DisplaySlot;
 
 public abstract class SpigotScoreboardView extends ScoreboardView {
@@ -14,5 +16,28 @@ public abstract class SpigotScoreboardView extends ScoreboardView {
         this.objective = scoreboard.getScoreboard().registerNewObjective(name);
         this.objective.setDisplayName(displayName);
         ((SpigotObjective) objective).getObjective().setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
+    
+    @Override
+    public ITeam createTeam(String teamName, String entry, int score) {
+        return createTeam(teamName, entry, null, null, score);
+    }
+    
+    @Override
+    public ITeam createTeam(String teamName, String entry, String prefix, int score) {
+        return createTeam(teamName, entry, prefix, null, score);
+    }
+    
+    @Override
+    public ITeam createTeam(String teamName, String entry, String prefix, String suffix, int score) {
+        ITeam team = this.scoreboard.getScoreboard().registerNewTeam(teamName);
+        if (prefix != null) {
+            team.setPrefix(MCUtils.color(prefix));
+        }
+        if (suffix != null) {
+            team.setSuffix(MCUtils.color(suffix));
+        }
+        addEntry(objective, team, MCUtils.color(entry), score);
+        return team;
     }
 }
