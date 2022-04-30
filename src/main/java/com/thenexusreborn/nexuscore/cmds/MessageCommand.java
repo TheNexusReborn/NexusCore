88741ded8f08ca760c740e85e381392a229b2e53
Firestar311 <slaterandrew0398@gmail.com>
2022-04-30 01:27:31 -0500

    Fix ender chests

--- src/main/java/com/thenexusreborn/nexuscore/cmds/MessageCommand.java
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/MessageCommand.java
@@ -1,7 +1,13 @@
 package com.thenexusreborn.nexuscore.cmds;
 
+import com.thenexusreborn.api.NexusAPI;
+import com.thenexusreborn.api.player.NexusPlayer;
 import com.thenexusreborn.nexuscore.NexusCore;
+import com.thenexusreborn.nexuscore.util.MCUtils;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class MessageCommand implements CommandExecutor {
     
@@ -13,6 +19,25 @@ public class MessageCommand implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MCUtils.color("&cOnly players may use that command."));
+            return true;
+        }
+        
+        if (!(args.length > 1)) {
+            sender.sendMessage(MCUtils.color("&cUsage: /message <player> <text>"));
+            return true;
+        }
+        
+        Player senderPlayer = (Player) sender;
+        NexusPlayer player = NexusAPI.getApi().getPlayerManager().getNexusPlayer(senderPlayer.getUniqueId());
+        
+        NexusPlayer target;
+        try {
+            target = NexusAPI.getApi().getPlayerManager().getNexusPlayer(UUID.fromString(args[0]));
+        } catch (Exception e) {
+            target = NexusAPI.getApi().getPlayerManager().getNexusPlayer(args[0]);
+        }
         
         return true;
     }
