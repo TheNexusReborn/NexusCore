fa4b7b6647cc6fda2d37b4cdcdb36e0d77a9ce9c
Firestar311 <slaterandrew0398@gmail.com>
2022-04-30 03:38:51 -0500

    Added tracking for when the last notification for playtime xp was

--- src/main/java/com/thenexusreborn/nexuscore/player/SpigotNexusPlayer.java
+++ src/main/java/com/thenexusreborn/nexuscore/player/SpigotNexusPlayer.java
@@ -18,8 +18,8 @@ public final class SpigotNexusPlayer extends NexusPlayer {
         super(uniqueId, name);
     }
     
-    public SpigotNexusPlayer(UUID uniqueId, Map<Rank, Long> ranks, long firstJoined, long lastLogin, long lastLogout, long playTime, String lastKnownName, Tag tag, Set<Tag> unlockedTags) {
-        super(uniqueId, ranks, firstJoined, lastLogin, lastLogout, playTime, lastKnownName, tag, unlockedTags);
+    public SpigotNexusPlayer(UUID uniqueId, Map<Rank, Long> ranks, long firstJoined, long lastLogin, long lastLogout, long playTime, String lastKnownName, Tag tag, Set<Tag> unlockedTags, int lastPlaytimeNotification) {
+        super(uniqueId, ranks, firstJoined, lastLogin, lastLogout, playTime, lastKnownName, tag, unlockedTags, lastPlaytimeNotification);
     }
     
     @Override
