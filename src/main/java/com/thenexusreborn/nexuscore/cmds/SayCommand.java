c28325fdf7856e48126cac43dc72667342199c78
Firestar311 <slaterandrew0398@gmail.com>
2022-04-30 07:04:53 -0500

    Added a say command

--- src/main/java/com/thenexusreborn/nexuscore/cmds/SayCommand.java
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/SayCommand.java
@@ -1,6 +1,9 @@
 package com.thenexusreborn.nexuscore.cmds;
 
+import com.thenexusreborn.api.player.Rank;
 import com.thenexusreborn.nexuscore.NexusCore;
+import com.thenexusreborn.nexuscore.util.MCUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 
 public class SayCommand implements CommandExecutor {
@@ -13,7 +16,18 @@ public class SayCommand implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        
+        Rank senderRank = MCUtils.getSenderRank(plugin, sender);
+        if (senderRank.ordinal() > Rank.HELPER.ordinal()) {
+            sender.sendMessage(MCUtils.color("&cYou do not have permission to use that command."));
+            return true;
+        }
+    
+        StringBuilder sb = new StringBuilder();
+        for (String arg : args) {
+            sb.append(arg).append(" ");
+        }
+    
+        Bukkit.broadcastMessage(MCUtils.color("&8[&f&l&oSAY&8] " + senderRank.getColor() + sender.getName() + "&8: &b" + sb));
         return true;
     }
 }
