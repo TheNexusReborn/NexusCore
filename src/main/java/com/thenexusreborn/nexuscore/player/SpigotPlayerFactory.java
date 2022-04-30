c07670fa90fdacf28c9269572aad120359a90b5f
Firestar311 <slaterandrew0398@gmail.com>
2022-04-30 03:43:57 -0500

    Revert "Added tracking for when the last notification for playtime xp was"
    This reverts commit fa4b7b6647cc6fda2d37b4cdcdb36e0d77a9ce9c.

--- src/main/java/com/thenexusreborn/nexuscore/player/SpigotPlayerFactory.java
+++ src/main/java/com/thenexusreborn/nexuscore/player/SpigotPlayerFactory.java
@@ -7,8 +7,8 @@ import java.util.*;
 
 public class SpigotPlayerFactory extends PlayerFactory {
     @Override
-    public NexusPlayer createPlayer(UUID uuid, Map<Rank, Long> ranks, long firstJoined, long lastLogin, long lastLogout, long playtime, String lastKnownName, Tag tag, Set<Tag> unlockedTags, int lastPlaytimeNotification) {
-        return new SpigotNexusPlayer(uuid, ranks, firstJoined, lastLogin, lastLogout, playtime, lastKnownName, tag, unlockedTags, lastPlaytimeNotification);
+    public NexusPlayer createPlayer(UUID uuid, Map<Rank, Long> ranks, long firstJoined, long lastLogin, long lastLogout, long playtime, String lastKnownName, Tag tag, Set<Tag> unlockedTags) {
+        return new SpigotNexusPlayer(uuid, ranks, firstJoined, lastLogin, lastLogout, playtime, lastKnownName, tag, unlockedTags);
     }
     
     @Override
