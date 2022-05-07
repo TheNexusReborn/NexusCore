3c0cd1ac7caa0416e7413110d14b99b0951e7ecf
Firestar311 <slaterandrew0398@gmail.com>
2022-05-06 19:57:22 -0500

    Reworked the admin command to make it better and more expandable

--- src/main/java/com/thenexusreborn/nexuscore/util/MsgType.java
+++ src/main/java/com/thenexusreborn/nexuscore/util/MsgType.java
@@ -1,28 +1,37 @@
 package com.thenexusreborn.nexuscore.util;
 
 public enum MsgType {
-    INFO("&6&l>>&e "),
-    SUCCESS("&6&l>>&a "),
-    IMPORTANT("&2&l>>&a "),
-    WARN("&6&l>>&c "),
-    ERROR("&4&l>>&7 "),
-    SEVERE("&4&l>>&4 "),
-    DETAIL("&d&l>>&7 "),
-    VERBOSE("&6&l>>&7&o ");
+    INFO("&6", "&e", "&b"),
+    SUCCESS("&6", "&a", "&b"),
+    IMPORTANT("&2", "&a", "&b"),
+    WARN("&6", "&c", "&e"),
+    ERROR("&4", "&7", "&c"),
+    SEVERE("&4", "&4", "&c"),
+    DETAIL("&d", "&7", "&e"),
+    VERBOSE("&6", "&7&o", "&e&o");
 
-    private final String prefix;
+    private final String prefixColor, baseColor, variableColor;
 
-    MsgType(String prefix){
-        this.prefix = prefix;
+    MsgType(String prefixColor, String baseColor, String variableColor) {
+        this.prefixColor = prefixColor;
+        this.baseColor = baseColor;
+        this.variableColor = variableColor;
     }
-
-    public String getPrefix(){
-        return prefix;
+    
+    public String getPrefixColor() {
+        return prefixColor;
     }
-
-
+    
+    public String getBaseColor() {
+        return baseColor;
+    }
+    
+    public String getVariableColor() {
+        return variableColor;
+    }
+    
     @Override
     public String toString(){
-        return prefix;
+        return prefixColor + "&l>> " + baseColor;
     }
 }
