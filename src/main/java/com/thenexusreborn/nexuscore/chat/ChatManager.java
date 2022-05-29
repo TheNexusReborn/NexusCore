40754963c5752f19227bb0286d421356bd949457
Firestar311 <slaterandrew0398@gmail.com>
2022-05-29 15:00:17 -0500

    Removed the double percent sign in messages

--- src/main/java/com/thenexusreborn/nexuscore/chat/ChatManager.java
+++ src/main/java/com/thenexusreborn/nexuscore/chat/ChatManager.java
@@ -52,14 +52,14 @@ public class ChatManager implements Listener {
             }
         }
         
-        String format = "&8(&2&l{level}&8) &r" + nexusPlayer.getDisplayName() + "{tag}&8: " + chatColor + ChatColor.stripColor(MCUtils.color(e.getMessage().replace("%", "%%")));
+        String format = "&8(&2&l{level}&8) &r" + nexusPlayer.getDisplayName() + "{tag}&8: " + chatColor + ChatColor.stripColor(MCUtils.color(e.getMessage()));
         format = format.replace("{level}", nexusPlayer.getLevel() + "");
         if (nexusPlayer.getTag() != null) {
             format = format.replace("{tag}", " " + nexusPlayer.getTag().getDisplayName());
         } else {
             format = format.replace("{tag}", "");
         }
-        e.setFormat(MCUtils.color(format));
+        e.setFormat(MCUtils.color(format.replace("%", "%%")));
     }
     
     public void setHandler(ChatHandler handler) {
