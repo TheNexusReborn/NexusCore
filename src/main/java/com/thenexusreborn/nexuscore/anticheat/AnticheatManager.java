f2322f4fa17fc243348790fdfd5d314ba4e34ce1
Firestar311 <slaterandrew0398@gmail.com>
2022-06-06 18:09:44 -0500

    New api stuff for spartan

--- src/main/java/com/thenexusreborn/nexuscore/anticheat/AnticheatManager.java
+++ src/main/java/com/thenexusreborn/nexuscore/anticheat/AnticheatManager.java
@@ -4,6 +4,7 @@ import com.thenexusreborn.api.*;
 import com.thenexusreborn.api.punishment.*;
 import com.thenexusreborn.nexuscore.util.MCUtils;
 import me.vagdedes.spartan.api.*;
+import me.vagdedes.spartan.objects.system.Check;
 import org.bukkit.event.*;
 
 public class AnticheatManager implements Listener {
@@ -13,23 +14,27 @@ public class AnticheatManager implements Listener {
         if (e.isCancelled()) {
             return;
         }
+        
         if (!e.isFalsePositive()) {
-            if (e.getViolation() % 10 == 0) {
-                StaffChat.sendAnticheat(NexusAPI.getApi().getPlayerManager().getNexusPlayer(e.getPlayer().getUniqueId()), e.getHackType().toString(), e.getViolation());
-            }
-    
-            if (e.getViolation() >= 30 || API.getVL(e.getPlayer()) >= 30) {
-                if (e.getViolation() >= 30) {
-                    NexusAPI.getApi().getLogger().info("Banned for a single violation count");
-                } else {
-                    NexusAPI.getApi().getLogger().info("Banned for multiple violation counts");
+            String category = API.getCategory(e.getPlayer(), e.getHackType());
+            if (category.equals(Check.maximumCategory)) {
+                if (e.getViolation() % 10 == 0) {
+                    StaffChat.sendAnticheat(NexusAPI.getApi().getPlayerManager().getNexusPlayer(e.getPlayer().getUniqueId()), e.getHackType().toString(), e.getViolation());
                 }
     
-                Punishment punishment = new Punishment(System.currentTimeMillis(), 2592000000L, "PowerMoveRegulator", e.getPlayer().getUniqueId().toString(), 
-                        NexusAPI.getApi().getServerManager().getCurrentServer().getName(), "Cheating", PunishmentType.BAN, Visibility.SILENT);
-                
-                NexusAPI.getApi().getDataManager().pushPunishment(punishment);
-                e.getPlayer().kickPlayer(MCUtils.color(punishment.formatKick()));
+                if (e.getViolation() >= 30 || API.getVL(e.getPlayer()) >= 30) {
+                    if (e.getViolation() >= 30) {
+                        NexusAPI.getApi().getLogger().info("Banned for a single violation count");
+                    } else {
+                        NexusAPI.getApi().getLogger().info("Banned for multiple violation counts");
+                    }
+        
+                    Punishment punishment = new Punishment(System.currentTimeMillis(), 2592000000L, "PowerMoveRegulator", e.getPlayer().getUniqueId().toString(),
+                            NexusAPI.getApi().getServerManager().getCurrentServer().getName(), "Cheating", PunishmentType.BAN, Visibility.SILENT);
+        
+                    NexusAPI.getApi().getDataManager().pushPunishment(punishment);
+                    e.getPlayer().kickPlayer(MCUtils.color(punishment.formatKick()));
+                }
             }
         }
     }
