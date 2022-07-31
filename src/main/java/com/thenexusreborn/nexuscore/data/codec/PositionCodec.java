e6d29c84a8ce0b7666502ad8272ea5b24d3035f7
Firestar311 <slaterandrew0398@gmail.com>
2022-07-30 23:04:58 -0500

    Added a PositionCodec for positions and databases

---
+++ src/main/java/com/thenexusreborn/nexuscore/data/codec/PositionCodec.java
@@ -0,0 +1,27 @@
+package com.thenexusreborn.nexuscore.data.codec;
+
+import com.thenexusreborn.api.data.objects.SqlCodec;
+import com.thenexusreborn.nexuscore.util.Position;
+
+public class PositionCodec extends SqlCodec<Position> {
+    @Override
+    public String encode(Object object) {
+        Position position = (Position) object;
+        return position.getX() + "," + position.getY() + "," + position.getZ() + "," + position.getYaw() + "," + position.getPitch();
+    }
+    
+    @Override
+    public Position decode(String encoded) {
+        String[] split = encoded.split(",");
+        if (split == null || split.length != 5) {
+            return null;
+        }
+        
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        int z = Integer.parseInt(split[2]);
+        float yaw = Float.parseFloat(split[3]);
+        float pitch = Float.parseFloat(split[4]);
+        return new Position(x, y, z, yaw, pitch);
+    }
+}
