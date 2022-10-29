343f31e786d7c42e0ff44d85b731428b719645a2
Firestar311 <slaterandrew0398@gmail.com>
2022-10-29 01:51:36 -0500

    Removed the JavaDocs and added a data variable (for colored things like glass and wool)

--- src/main/java/com/thenexusreborn/nexuscore/util/builder/ItemBuilder.java
+++ src/main/java/com/thenexusreborn/nexuscore/util/builder/ItemBuilder.java
@@ -11,11 +11,6 @@ import org.bukkit.potion.*;
 
 import java.util.*;
 
-/**
- * A class to make creating items better
- * <p> 
- * This does check to see if the settings and values are valid for the item type and is safe to use.
- */
 public class ItemBuilder {
     private String displayName;
     private List<String> lore = new LinkedList<>();
@@ -32,56 +27,30 @@ public class ItemBuilder {
     private int repairCost = -1;
     private UUID skullOwner;
     private int amount = 1, durability;
+    private short data;
     private Material material;
     
-    /**
-     * Constructs a new ItemBuilder using a Material
-     * @param material The starting Material
-     */
     public ItemBuilder(Material material) {
         this.material = material;
     }
     
-    /**
-     * Constructs an ItemBuilder that is empty
-     */
     public ItemBuilder() {}
     
-    /**
-     * Constructs an ItemBuilder with a starting material and amount
-     * @param material The material
-     * @param amount The amount
-     */
     public ItemBuilder(Material material, int amount) {
         this.material = material;
         this.amount = amount;
     }
     
-    /**
-     * The static method of the constructor
-     * @param material The material
-     * @return The created ItemBuilder
-     */
     public static ItemBuilder start(Material material) {
         return new ItemBuilder(material);
     }
     
-    /**
-     * The static method of the con structor
-     * @param material The material
-     * @param amount The amount
-     * @return The created ItemBuilder
-     */
     public static ItemBuilder start(Material material, int amount) {
         return new ItemBuilder(material, amount);
     }
     
-    /**
-     * Builds the ItemStack based on the builder
-     * @return The ItemStack
-     */
     public ItemStack build() {
-        ItemStack itemStack = new ItemStack(material, amount);
+        ItemStack itemStack = new ItemStack(material, amount, data);
         ItemMeta itemMeta = itemStack.getItemMeta();
         if (displayName != null) {
             itemMeta.setDisplayName(MCUtils.color(displayName));
@@ -178,253 +147,131 @@ public class ItemBuilder {
         return itemStack;
     }
     
-    /** 
-     * Sets the material
-     * @param material The material
-     * @return This ItemBuilder
-     */
+    public ItemBuilder data(short data) {
+        this.data = data;
+        return this;
+    }
+    
     public ItemBuilder material(Material material) {
         this.material = material;
         return this;
     }
     
-    /**
-     * Sets the DisplayName
-     * @param displayName The new display name
-     * @return This ItemBuilder
-     */
     public ItemBuilder displayName(String displayName) {
         this.displayName = displayName;
         return this;
     }
     
-    /**
-     * Sets the lore
-     * @param lore The lore
-     * @return This ItemBuilder
-     */
     public ItemBuilder lore(List<String> lore) {
         this.lore = lore;
         return this;
     }
     
-    /**
-     * Adds a line to the exisiting lore
-     * @param line The line to add
-     * @return This ItemBuilder
-     */
     public ItemBuilder addLoreLine(String line) {
         this.lore.add(line);
         return this;
     }
     
-    /**
-     * Sets the current enchantments
-     * @param enchantments The enchantments to set
-     * @return This ItemBuilder
-     */
     public ItemBuilder enchantments(Map<Enchantment, Integer> enchantments) {
         this.enchantments = enchantments;
         return this;
     }
     
-    /**
-     * Adds an enchantment to the current list of enchantments
-     * @param enchantment The enchantment
-     * @param level The level of the enchantment
-     * @return This ItemBuilder
-     */
     public ItemBuilder addEnchantment(Enchantment enchantment, int level) {
         this.enchantments.put(enchantment, level);
         return this;
     }
     
-    /**
-     * Sets the stored enchantments, this is for items that store enchantments like Enchanted Books
-     * @param storedEnchantments The enchantments
-     * @return This ItemBuilder
-     */
     public ItemBuilder storedEnchantments(Map<Enchantment, Integer> storedEnchantments) {
         this.storedEnchantments = storedEnchantments;
         return this;
     }
     
-    /**
-     * Adds a stored enchantment
-     * @param enchantment The enchantment
-     * @param level The level of the Enchantment
-     * @return This ItemBuilder
-     */
     public ItemBuilder addStoredEnchantment(Enchantment enchantment, int level) {
         this.storedEnchantments.put(enchantment, level);
         return this;
     }
     
-    /**
-     * Sets the ItemFlags
-     * @param itemFlags The ItemFlags
-     * @return This ItemBuilder
-     */
     public ItemBuilder itemFlags(Set<ItemFlag> itemFlags) {
         this.itemFlags = itemFlags;
         return this;
     }
     
-    /**
-     * Adds an ItemFlag
-     * @param flags The ItemFlag(s) vararg
-     * @return This ItemBuilder
-     */
     public ItemBuilder addItemFlag(ItemFlag... flags) {
         this.itemFlags.addAll(Arrays.asList(flags));
         return this;
     }
     
-    /**
-     * Sets the unbreakable value 
-     * @param unbreakable The value
-     * @return This ItemBuilder
-     */
     public ItemBuilder unbreakable(boolean unbreakable) {
         this.unbreakable = unbreakable;
         return this;
     }
     
-    /**
-     * Sets the Banner patterns
-     * @param bannerPatterns The banner patterns
-     * @return This ItemBuilder
-     */
     public ItemBuilder bannerPatterns(List<Pattern> bannerPatterns) {
         this.bannerPatterns = bannerPatterns;
         return this;
     }
     
-    /**
-     * Sets the BlockState (If it is a block)
-     * @param blockState The state to set
-     * @return This ItemBuilder
-     */
     public ItemBuilder blockState(BlockState blockState) {
         this.blockState = blockState;
         return this;
     }
     
-    /**
-     * Sets the book title
-     * @param bookTitle The title of the book
-     * @return This ItemBuilder
-     */
     public ItemBuilder bookTitle(String bookTitle) {
         this.bookTitle = bookTitle;
         return this;
     }
     
-    /**
-     * Sets the Author of the book
-     * @param bookAuthor The author
-     * @return This ItemBuilder
-     */
     public ItemBuilder bookAuthor(String bookAuthor) {
         this.bookAuthor = bookAuthor;
         return this;
     }
     
-    /**
-     * Sets the pages of the book
-     * @param bookPages The pages
-     * @return This ItemBuilder
-     */
     public ItemBuilder bookPages(List<String> bookPages) {
         this.bookPages = bookPages;
         return this;
     }
     
-    /**
-     * Sets the color of the armor
-     * @param armorColor The color
-     * @return This ItemBuilder
-     */
     public ItemBuilder armorColor(Color armorColor) {
         this.armorColor = armorColor;
         return this;
     }
     
-    /**
-     * Sets the Main Potion Effect Type
-     * @param mainEffect The effect
-     * @return This ItemBuilder
-     */
     public ItemBuilder mainEffect(PotionEffectType mainEffect) {
         this.mainEffect = mainEffect;
         return this;
     }
     
-    /**
-     * Sets the potion effects
-     * @param potionEffects The potion effects
-     * @return This ItemBuilder
-     */
     public ItemBuilder potionEffects(List<PotionEffect> potionEffects) {
         this.potionEffects = potionEffects;
         return this;
     }
     
-    /**
-     * Adds a PotionEffect
-     * @param potionEffects The potion effect varargs
-     * @return This ItemBuilder
-     */
     public ItemBuilder addPotionEffects(PotionEffect... potionEffects) {
         this.potionEffects.addAll(Arrays.asList(potionEffects));
         return this;
     }
     
-    /**
-     * Sets the repair cost
-     * @param repairCost The new repair cost
-     * @return This ItemBuilder
-     */
     public ItemBuilder repairCost(int repairCost) {
         this.repairCost = repairCost;
         return this;
     }
     
-    /**
-     * Sets the Skull Owner
-     * @param skullOwner The UUID of the player
-     * @return This ItemBuilder
-     */
     public ItemBuilder skullOwner(UUID skullOwner) {
         this.skullOwner = skullOwner;
         return this;
     }
     
-    /**
-     * Sets the Amount of the itemstack
-     * @param amount The amount
-     * @return This ItemBuilder
-     */
     public ItemBuilder amount(int amount) {
         this.amount = amount;
         return this;
     }
     
-    /**
-     * Sets the durability of the item
-     * @param durability The durability
-     * @return This ItemBuilder
-     */
     public ItemBuilder durability(int durability) {
         this.durability = durability;
         return this;
     }
     
-    /**
-     * Sets the lore in a varargs way
-     * @param lore The lore to set (varargs)
-     * @return This ItemBuilder
-     */
     public ItemBuilder lore(String... lore) {
         if (lore != null) {
             for (String s : lore) {
