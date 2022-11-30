308b82301138292a6c1669f303b53580bab9aef2
Firestar311 <slaterandrew0398@gmail.com>
2022-11-30 13:30:39 -0600

    Added some timer specific methods to the Timer class

--- src/main/java/com/thenexusreborn/nexuscore/clock/impl/Timer.java
+++ src/main/java/com/thenexusreborn/nexuscore/clock/impl/Timer.java
@@ -9,11 +9,40 @@ public class Timer extends Clock<TimerSnapshot> {
     
     public Timer(long length) {
         this.length = length;
+        this.time = length;
+    }
+    
+    public long getLength() {
+        return length;
+    }
+    
+    public void reset() {
+        this.time = length;
+    }
+    
+    public void setLength(long length) {
+        long elapsed = this.length - this.time;
+        this.length = length;
+        this.time = Math.max(this.length - elapsed, 0);
+    }
+    
+    public void addLength(long length) {
+        setLength(this.length + length);
+    }
+    
+    public void removeLength(long length) {
+        if (length - this.length < 0) {
+            length = this.length;
+        }
+        
+        setLength(this.length - length);
     }
     
     @Override
     public void count() {
-        this.time -= 50;
+        if (time >= 50) {
+            this.time -= 50;
+        }
     }
     
     @Override
