ca9003a55d3028cc854ed10b66397651463c1b0d
Firestar311 <slaterandrew0398@gmail.com>
2023-10-26 21:08:38 -0500

    Added a /list command

--- src/main/java/com/thenexusreborn/nexuscore/NexusCore.java
+++ src/main/java/com/thenexusreborn/nexuscore/NexusCore.java
@@ -75,6 +75,7 @@ public class NexusCore extends JavaPlugin {
         getCommand("message").setExecutor(new MessageCommand());
         getCommand("reply").setExecutor(new ReplyCommand());
         getCommand("me").setExecutor(new MeCommand());
+        getCommand("list").setExecutor(new ListCommand(this));
         getCommand("discord").setExecutor((sender, cmd, label, args) -> {
             sender.sendMessage(MCUtils.color(MsgType.INFO + "Discord: &bhttps://discord.gg/bawZKSWEpT"));
             return true;
---
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/ListCommand.java
@@ -0,0 +1,72 @@
+package com.thenexusreborn.nexuscore.cmds;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.thenexusreborn.api.NexusAPI;
+import com.thenexusreborn.api.helper.StringHelper;
+import com.thenexusreborn.api.player.NexusPlayer;
+import com.thenexusreborn.api.player.Rank;
+import com.thenexusreborn.nexuscore.NexusCore;
+import com.thenexusreborn.nexuscore.util.MCUtils;
+import com.thenexusreborn.nexuscore.util.MsgType;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ListCommand implements CommandExecutor {
+    
+    private NexusCore plugin;
+
+    public ListCommand(NexusCore plugin) {
+        this.plugin = plugin;
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Rank senderRank = MCUtils.getSenderRank(plugin, sender);
+        if (sender instanceof ConsoleCommandSender) {
+            senderRank = Rank.NEXUS; //Override to allow console to see nexus team, as only Nexus Team have console access
+        }
+        Multimap<Rank, NexusPlayer> playerList = HashMultimap.create();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            NexusPlayer nexusPlayer = NexusAPI.getApi().getPlayerManager().getNexusPlayer(player.getUniqueId());
+            if (nexusPlayer == null) {
+                continue; //Should never happen
+            }
+
+            if (nexusPlayer.getStatValue("vanish").getAsBoolean() || nexusPlayer.getStatValue("incognito").getAsBoolean()) {
+                if (nexusPlayer.getRank().ordinal() < senderRank.ordinal()) {
+                    continue;
+                }
+            }
+
+            playerList.put(nexusPlayer.getRank(), nexusPlayer);
+        }
+
+        sender.sendMessage(MCUtils.color(MsgType.INFO + "Players online on The Nexus Reborn."));
+        for (Map.Entry<Rank, Collection<NexusPlayer>> entry : playerList.asMap().entrySet()) {
+            Rank rank = entry.getKey();
+            Collection<NexusPlayer> players = entry.getValue();
+            Iterator<NexusPlayer> playersIterator = players.iterator();
+            StringBuilder sb = new StringBuilder();
+            while (playersIterator.hasNext()) {
+                NexusPlayer player = playersIterator.next();
+                sb.append(player.getName());
+                if (playersIterator.hasNext()) {
+                    sb.append(", ");
+                }
+            }
+            if (!sb.isEmpty()) {
+                sender.sendMessage(MCUtils.color("  &6&l> " + rank.getColor() + StringHelper.capitalizeEveryWord(rank.name().replace("_", " ")) + "&8: &f" + sb));
+            }
+        }
+        
+        return true;
+    }
+}
