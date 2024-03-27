1c6819803084985626a0ff6bfa7ec577c080213a
Firestar311 <slaterandrew0398@gmail.com>
2024-03-27 10:19:34 -0600

    Replaced the NexusThread wrapper class with the StarThread class (NexusThread did nothing special)

--- src/main/java/com/thenexusreborn/nexuscore/thread/PlayerLoadActionBarThread.java
+++ src/main/java/com/thenexusreborn/nexuscore/thread/PlayerLoadActionBarThread.java
@@ -1,14 +1,14 @@
 package com.thenexusreborn.nexuscore.thread;
 
+import com.stardevllc.starmclib.StarThread;
 import com.thenexusreborn.api.NexusAPI;
 import com.thenexusreborn.api.player.NexusPlayer;
 import com.thenexusreborn.nexuscore.NexusCore;
-import com.thenexusreborn.nexuscore.api.NexusThread;
 import com.thenexusreborn.nexuscore.util.SpigotUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public class PlayerLoadActionBarThread extends NexusThread<NexusCore> {
+public class PlayerLoadActionBarThread extends StarThread<NexusCore> {
 
     public PlayerLoadActionBarThread(NexusCore plugin) {
         super(plugin, 20L, 0L, false);
