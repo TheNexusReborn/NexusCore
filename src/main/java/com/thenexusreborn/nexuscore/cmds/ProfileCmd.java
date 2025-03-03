aae5ade475f7f9e724ffa7ffe0e12a7be02119b0
Firestar311 <slaterandrew0398@gmail.com>
2025-03-03 08:56:16 -0600

    Added more to the Profile Command

--- src/main/java/com/thenexusreborn/nexuscore/cmds/ProfileCmd.java
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/ProfileCmd.java
@@ -2,9 +2,13 @@ package com.thenexusreborn.nexuscore.cmds;
 
 import com.stardevllc.cmdflags.FlagResult;
 import com.stardevllc.colors.StarColors;
+import com.stardevllc.time.TimeFormat;
 import com.thenexusreborn.api.NexusAPI;
 import com.thenexusreborn.api.player.NexusPlayer;
+import com.thenexusreborn.api.player.PlayerToggles;
 import com.thenexusreborn.api.player.Rank;
+import com.thenexusreborn.api.player.Toggle;
+import com.thenexusreborn.api.tags.Tag;
 import com.thenexusreborn.nexuscore.NexusCore;
 import com.thenexusreborn.nexuscore.api.command.NexusCommand;
 import com.thenexusreborn.nexuscore.util.MsgType;
@@ -12,7 +16,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
 public class ProfileCmd extends NexusCommand<NexusCore> {
+    
+    public static final Set<Function<NexusPlayer, Map<String, Object>>> supplementals = new HashSet<>();
+    
     public ProfileCmd(NexusCore plugin) {
         super(plugin, "profile", "", Rank.HELPER);
     }
@@ -43,10 +57,75 @@ public class ProfileCmd extends NexusCommand<NexusCore> {
             MsgType.ERROR.send(sender, "Invalid target");
             return true;
         }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a z");
+        TimeFormat timeFormat = new TimeFormat("%*#0y %%*#0mo %%*#0d %%*#0h %%#0m %%00s%");
+        DecimalFormat numberFormat = new DecimalFormat("#,###,###,###.##");
         
         sender.sendMessage(StarColors.color("&6&l>> &eProfile information for &b" + target.getName()));
-        sender.sendMessage(StarColors.color("&6&l > &eServer: &f" + (target.getServer() != null ? target.getServer().getName() : "Not online")));
+        sendProfileLine(sender, "Server", (target.getServer() != null ? target.getServer().getName() : "Not online"));
+        sendProfileLine(sender, "Level", target.getExperience().getLevel());
+        sendProfileLine(sender, "Level XP", numberFormat.format(target.getExperience().getLevelXp()));
+        sendProfileLine(sender, "First Join", dateFormat.format(target.getPlayerTime().getFirstJoined()));
+        sendProfileLine(sender, "Last Login", dateFormat.format(target.getPlayerTime().getLastLogin()));
+        if (target.isOnline()) {
+            sendProfileLine(sender, "Last Logout", "&aOnline");
+        } else {
+            sendProfileLine(sender, "Last Logout", dateFormat.format(target.getPlayerTime().getLastLogout()));
+        }
         
+        sendProfileLine(sender, "Play Time", timeFormat.format(target.getPlayerTime().getPlaytime()));
+        sendProfileLine(sender, "Nexites", numberFormat.format(target.getBalance().getNexites()));
+        sendProfileLine(sender, "Credits", numberFormat.format(target.getBalance().getCredits()));
+
+        Rank rank = target.getRank();
+        String rankName = rank.getColor() + (rank.isBold() ? "&l" : "") + rank.name().replace("_", " ");
+        sendProfileLine(sender, "Primary Rank", rankName);
+
+        StringBuilder secondaryRanksBuilder = new StringBuilder();
+        Map<Rank, Long> ranks = target.getRanks().findAll();
+        if (ranks.size() > 2) {
+            ranks.forEach((r, expire) -> {
+                if (r == rank) {
+                    return;
+                }
+                
+                String rn = r.getColor() + (r.isBold() ? "&l" : "") + r.name().replace("_", " ");
+                secondaryRanksBuilder.append(rn).append(" ");
+            });
+            sendProfileLine(sender, "Secondary Ranks", secondaryRanksBuilder.toString());
+        }
+        
+        if (target.hasActiveTag()) {
+            sendProfileLine(sender, "Active Tag", target.getActiveTag().getDisplayName());
+        } else {
+            sendProfileLine(sender, "Active Tag", "Not Set");
+        }
+        
+        StringBuilder tagsBuilder = new StringBuilder();
+        for (String tag : target.getTags()) {
+            tagsBuilder.append(new Tag(null, tag, 0L).getDisplayName()).append(" ");
+        }
+        
+        sendProfileLine(sender, "Unlocked Tags", tagsBuilder.toString());
+
+        PlayerToggles toggles = target.getToggles();
+
+        for (Toggle.Info toggleInfo : NexusAPI.getApi().getToggleRegistry()) {
+            if (target.getRank().ordinal() <= toggleInfo.getMinRank().ordinal()) {
+                sendProfileLine(sender, toggleInfo.getDisplayName(), toggles.getValue(toggleInfo.getName()));
+            }
+        }
+
+        for (Function<NexusPlayer, Map<String, Object>> supplemental : supplementals) {
+            Map<String, Object> values = supplemental.apply(target);
+            values.forEach((k, v) -> sendProfileLine(sender, k, v));
+        }
+
         return true;
+    }
+    
+    private void sendProfileLine(CommandSender sender, String prefix, Object data) {
+        sender.sendMessage(StarColors.color(" &6&l> &e" + prefix + ": &f" + data.toString()));
     }
 }
