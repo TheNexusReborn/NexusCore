dbe712ff0d5b35bba22f8df19b1b6227b79f19bf
Firestar311 <slaterandrew0398@gmail.com>
2025-04-08 02:10:15 -0500

    Added some feedback messages to the /unnick command

--- src/main/java/com/thenexusreborn/nexuscore/cmds/UnnickCommand.java
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/UnnickCommand.java
@@ -7,6 +7,7 @@ import com.thenexusreborn.api.player.NexusPlayer;
 import com.thenexusreborn.api.player.Rank;
 import com.thenexusreborn.nexuscore.NexusCore;
 import com.thenexusreborn.nexuscore.api.command.NexusCommand;
+import com.thenexusreborn.nexuscore.util.MsgType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -20,12 +21,17 @@ public class UnnickCommand extends NexusCommand<NexusCore> {
     @Override
     public boolean execute(CommandSender sender, Rank senderRank, String label, String[] args, FlagResult flagResults) {
         NexusPlayer nexusPlayer = NexusAPI.getApi().getPlayerManager().getNexusPlayer(((Player) sender).getUniqueId());
+        
+        if (nexusPlayer.getNickname() == null) {
+            MsgType.WARN.send(sender, "You do not have a nickname set.");
+            return true;
+        }
+        
         SkinManager skinManager = Bukkit.getServicesManager().getRegistration(SkinManager.class).getProvider();
         plugin.getNickWrapper().setNick(plugin, ((Player) sender), nexusPlayer.getTrueName(), skinManager.getFromMojang(nexusPlayer.getUniqueId()));
         nexusPlayer.setNickname(null);
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            NexusAPI.getApi().getPrimaryDatabase().saveSilent(nexusPlayer);
-        });
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> NexusAPI.getApi().getPrimaryDatabase().saveSilent(nexusPlayer));
+        MsgType.INFO.send(sender, "You successfully unnicked yourself.");
         return true;
     }
 }
