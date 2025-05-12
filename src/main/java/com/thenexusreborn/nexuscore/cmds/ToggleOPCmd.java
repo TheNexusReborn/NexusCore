fd411c60f4879c0fc845b67fab226c75450bf001
Firestar311 <slaterandrew0398@gmail.com>
2025-05-11 19:23:57 -0500

    Added a ToggleOp Command for Nexus Team Members

---
+++ src/main/java/com/thenexusreborn/nexuscore/cmds/ToggleOPCmd.java
@@ -0,0 +1,32 @@
+package com.thenexusreborn.nexuscore.cmds;
+
+import com.stardevllc.starcore.cmdflags.FlagResult;
+import com.thenexusreborn.api.player.Rank;
+import com.thenexusreborn.nexuscore.NexusCore;
+import com.thenexusreborn.nexuscore.api.command.NexusCommand;
+import com.thenexusreborn.nexuscore.util.MsgType;
+import org.bukkit.command.CommandSender;
+
+public class ToggleOPCmd extends NexusCommand<NexusCore> {
+    public ToggleOPCmd(NexusCore plugin) {
+        super(plugin, "toggleop", "Toggling of OP status", Rank.NEXUS);
+        this.playerOnly = true;
+    }
+    
+    @Override
+    public boolean execute(CommandSender sender, Rank senderRank, String label, String[] args, FlagResult flagResults) {
+        if (senderRank != Rank.NEXUS) {
+            return true;
+        }
+        
+        if (sender.isOp()) {
+            sender.setOp(false);
+            MsgType.INFO.send(sender, "You are no longer a server operator.");
+        } else {
+            sender.setOp(true);
+            MsgType.INFO.send(sender, "You are now a server operator.");
+        }
+        
+        return true;
+    }
+}
