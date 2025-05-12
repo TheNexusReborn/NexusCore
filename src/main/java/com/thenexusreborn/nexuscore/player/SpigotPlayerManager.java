3ed9839acfdfc4e15ed3b439a817450a6d73c8b9
Firestar311 <slaterandrew0398@gmail.com>
2025-05-12 11:50:01 -0500

    Reworked some things in PlayerManagers to allow single and multi network setups without having to change it again

--- src/main/java/com/thenexusreborn/nexuscore/player/SpigotPlayerManager.java
+++ src/main/java/com/thenexusreborn/nexuscore/player/SpigotPlayerManager.java
@@ -9,21 +9,19 @@ import com.stardevllc.starcore.skins.Skin;
 import com.stardevllc.starcore.skins.SkinManager;
 import com.stardevllc.time.TimeUnit;
 import com.thenexusreborn.api.NexusAPI;
-import com.thenexusreborn.api.gamearchive.GameInfo;
 import com.thenexusreborn.api.nickname.Nickname;
 import com.thenexusreborn.api.player.*;
 import com.thenexusreborn.api.punishment.Punishment;
 import com.thenexusreborn.api.scoreboard.NexusScoreboard;
 import com.thenexusreborn.api.scoreboard.ScoreboardView;
-import com.thenexusreborn.api.sql.objects.*;
 import com.thenexusreborn.api.util.Constants;
 import com.thenexusreborn.api.util.NetworkType;
 import com.thenexusreborn.nexuscore.NexusCore;
 import com.thenexusreborn.nexuscore.api.events.NexusPlayerLoadEvent;
+import com.thenexusreborn.nexuscore.reflection.impl.ActionBar;
 import com.thenexusreborn.nexuscore.scoreboard.SpigotNexusScoreboard;
 import com.thenexusreborn.nexuscore.scoreboard.impl.RankTablistHandler;
 import com.thenexusreborn.nexuscore.util.MsgType;
-import com.thenexusreborn.nexuscore.util.SpigotUtils;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -35,12 +33,14 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.net.InetSocketAddress;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
-import java.util.List;
 import java.util.UUID;
 
 public class SpigotPlayerManager extends PlayerManager implements Listener {
+    
+    private static final DecimalFormat PLAYTIME_XP_FORMAT = new DecimalFormat(Constants.NUMBER_FORMAT);
 
     private final NexusCore plugin;
+    
 
     public SpigotPlayerManager(NexusCore plugin) {
         this.plugin = plugin;
@@ -73,135 +73,149 @@ public class SpigotPlayerManager extends PlayerManager implements Listener {
                 player.hidePlayer(o);
             }
         }
-
-        if (NexusAPI.NETWORK_TYPE == NetworkType.SINGLE) {
-            Session session = new Session(player.getUniqueId());
-            session.start();
-
-            if (!getPlayers().containsKey(player.getUniqueId())) {
-                Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-                    NexusPlayer nexusPlayer;
-                    if (!getUuidNameMap().containsKey(player.getUniqueId())) {
-                        nexusPlayer = createPlayerData(player.getUniqueId(), player.getName());
-                    } else {
-                        try {
-                            nexusPlayer = NexusAPI.getApi().getPrimaryDatabase().get(NexusPlayer.class, "uniqueId", player.getUniqueId()).getFirst();
-                        } catch (SQLException ex) {
-                            nexusPlayer = null;
-                            ex.printStackTrace();
-                        }
-                    }
-
-                    if (Bukkit.getPlayer(nexusPlayer.getUniqueId()) == null) {
+        
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            final NexusPlayer nexusPlayer;
+            if (!this.players.containsKey(player.getUniqueId())) {
+                if (!getUuidNameMap().containsKey(player.getUniqueId())) {
+                    nexusPlayer = createPlayerData(player.getUniqueId(), player.getName());
+                    NexusAPI.getApi().getPrimaryDatabase().saveSilent(nexusPlayer);
+                } else {
+                    try {
+                        nexusPlayer = NexusAPI.getApi().getPrimaryDatabase().get(NexusPlayer.class, "uniqueId", player.getUniqueId()).getFirst();
+                    } catch (Throwable ex) {
+                        ex.printStackTrace();
                         return;
                     }
-                    
-                    MojangProfile mojangProfile = MojangAPI.getProfile(player.getUniqueId());
-                    nexusPlayer.setMojangProfile(mojangProfile);
-
-                    nexusPlayer.setSession(session);
-                    getUuidNameMap().forcePut(nexusPlayer.getUniqueId(), new Name(nexusPlayer.getName()));
-                    getUuidRankMap().put(nexusPlayer.getUniqueId(), nexusPlayer.getRanks());
-
-                    if (nexusPlayer.getFirstJoined() == 0) {
-                        nexusPlayer.setFirstJoined(System.currentTimeMillis());
-                    }
-                    nexusPlayer.setLastLogin(System.currentTimeMillis());
-                    if (!nexusPlayer.getName().equals(player.getName())) {
-                        nexusPlayer.setName(player.getName());
-                    }
-
-                    try {
-                        NexusAPI.getApi().getPrimaryDatabase().save(nexusPlayer);
-                    } catch (SQLException ex) {
-                        player.sendMessage(StarColors.color(MsgType.ERROR + "Failed to save your player data to the database. Please report as a bug and try to re-log."));
-                        ex.printStackTrace();
-                    }
-
-                    getPlayers().put(nexusPlayer.getUniqueId(), nexusPlayer);
-                    InetSocketAddress socketAddress = player.getAddress();
-                    String hostName = socketAddress.getHostString();
-                    NexusAPI.getApi().getPlayerManager().addIpHistory(player.getUniqueId(), hostName);
-                    
-                    Nickname nickname = nexusPlayer.getNickname();
-                    SkinManager skinManager = Bukkit.getServicesManager().getRegistration(SkinManager.class).getProvider();
-                    Skin skin = (nickname != null && nickname.getSkin() != null && !nickname.getSkin().isBlank()) ? skinManager.getFromMojang(nickname.getSkin()) : null;
-
-                    NexusPlayer finalNexusPlayer = nexusPlayer;
-                    Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                        if (skin != null && nickname.isActive()) {
-                            plugin.getNickWrapper().setNick(plugin, player, nickname.getName(), skin);
-                        }
-                        
-                        NexusScoreboard scoreboard = new SpigotNexusScoreboard(finalNexusPlayer);
-                        scoreboard.init();
-
-                        finalNexusPlayer.setScoreboard(scoreboard);
-
-                        NexusPlayerLoadEvent loadEvent = new NexusPlayerLoadEvent(finalNexusPlayer);
-                        try {
-                            Bukkit.getServer().getPluginManager().callEvent(loadEvent);
-                        } catch (Exception ex) {
-                            plugin.getLogger().severe("There was an error handling the NexusPlayerLoadEvent. This was caught to prevent other issues. Stack Trace Below");
-                            ex.printStackTrace();
-                        }
-
-                        ScoreboardView scoreboardView = loadEvent.getScoreboardView();
-                        if (scoreboardView != null) {
-                            scoreboard.setView(scoreboardView);
-                        }
-
-                        if (scoreboard.getTablistHandler() == null) {
-                            if (loadEvent.getTablistHandler() != null) {
-                                scoreboard.setTablistHandler(loadEvent.getTablistHandler());
-                            } else {
-                                scoreboard.setTablistHandler(new RankTablistHandler(scoreboard));
-                            }
-                        }
-
-                        scoreboard.apply();
-
-                        String joinMessage = loadEvent.getJoinMessage();
-                        if (joinMessage != null && !joinMessage.isEmpty()) {
-                            Bukkit.broadcastMessage(StarColors.color(joinMessage));
-                        }
-
-                        NexusAPI.getApi().getPlayerManager().getPlayers().put(finalNexusPlayer.getUniqueId(), finalNexusPlayer);
-
-                        SpigotUtils.sendActionBar(player, "&aYour data has been loaded");
-
-                        plugin.getNexusServer().join(finalNexusPlayer);
-
-                        Stopwatch playtimeStopwatch = plugin.getClockManager().createStopwatch(Long.MAX_VALUE);
-                        playtimeStopwatch.addRepeatingCallback(stopwatchSnapshot -> {
-                            if (finalNexusPlayer.getToggleValue("vanish")) {
-                                return;
-                            }
-
-                            if (stopwatchSnapshot.getTime() == 0) {
-                                return; //Prevent this running immediately when a player joins
-                            }
-
-                            double xp = 10;
-
-                            DecimalFormat format = new DecimalFormat(Constants.NUMBER_FORMAT);
-
-                            finalNexusPlayer.sendMessage(MsgType.INFO + "&d+" + format.format(xp) + "XP (Playtime)");
-                            finalNexusPlayer.addXp(xp);
-                        }, TimeUnit.MINUTES.toMillis(10));
-                        playtimeStopwatch.start();
-
-                        if (loadEvent.getActionBar() != null) {
-                            new BukkitRunnable() {
-                                public void run() {
-                                    finalNexusPlayer.setActionBar(loadEvent.getActionBar());
-                                }
-                            }.runTaskLater(plugin, 60L);
-                        }
-                    }, 1L);
-                });
+                }
+            } else {
+                nexusPlayer = this.players.get(player.getUniqueId());
             }
-        }
+            
+            if (nexusPlayer == null || Bukkit.getPlayer(nexusPlayer.getUniqueId()) == null) {
+                return;
+            }
+            
+            getPlayers().put(nexusPlayer.getUniqueId(), nexusPlayer);
+            
+            Session session;
+            if (NexusAPI.NETWORK_TYPE == NetworkType.SINGLE) {
+                session = new Session(player.getUniqueId());
+                session.start();
+            } else {
+                try {
+                    session = NexusAPI.getApi().getPrimaryDatabase().get(Session.class, "uniqueid", player.getUniqueId()).getFirst();
+                } catch (Throwable ex) {
+                    session = new Session(player.getUniqueId());
+                    session.start();
+                    NexusAPI.getApi().getPrimaryDatabase().saveSilent(session);
+                }
+            }
+            
+            nexusPlayer.setSession(session);
+            
+            MojangProfile mojangProfile = MojangAPI.getProfile(player.getUniqueId());
+            nexusPlayer.setMojangProfile(mojangProfile);
+            
+            getUuidNameMap().forcePut(nexusPlayer.getUniqueId(), new Name(nexusPlayer.getName()));
+            getUuidRankMap().put(nexusPlayer.getUniqueId(), nexusPlayer.getRanks());
+            
+            if (nexusPlayer.getFirstJoined() == 0) {
+                nexusPlayer.setFirstJoined(session.getStart());
+            }
+            
+            nexusPlayer.setLastLogin(session.getStart());
+            
+            NexusAPI.getApi().getPrimaryDatabase().saveSilent(nexusPlayer);
+            
+            if (!nexusPlayer.getName().equals(player.getName())) {
+                nexusPlayer.setName(player.getName());
+            }
+            
+            try {
+                NexusAPI.getApi().getPrimaryDatabase().save(nexusPlayer);
+            } catch (SQLException ex) {
+                player.sendMessage(StarColors.color(MsgType.ERROR + "Failed to save your player data to the database. Please report as a bug and try to re-log."));
+                ex.printStackTrace();
+            }
+            
+            Stopwatch playtimeStopwatch = plugin.getClockManager().createStopwatch(Long.MAX_VALUE);
+            playtimeStopwatch.addRepeatingCallback(stopwatchSnapshot -> {
+                if (nexusPlayer.getToggleValue("vanish")) {
+                    return;
+                }
+                
+                if (stopwatchSnapshot.getTime() == 0) {
+                    return; //Prevent this running immediately when a player joins
+                }
+                
+                double xp = 10;
+                
+                nexusPlayer.sendMessage(MsgType.INFO + "&d+" + PLAYTIME_XP_FORMAT.format(xp) + "XP (Playtime)");
+                nexusPlayer.addXp(xp);
+            }, TimeUnit.MINUTES.toMillis(10));
+            playtimeStopwatch.start();
+            
+            InetSocketAddress socketAddress = player.getAddress();
+            String hostName = socketAddress.getHostString();
+            NexusAPI.getApi().getPlayerManager().addIpHistory(player.getUniqueId(), hostName);
+            
+            Nickname nickname = nexusPlayer.getNickname();
+            SkinManager skinManager = Bukkit.getServicesManager().getRegistration(SkinManager.class).getProvider();
+            Skin skin = (nickname != null && nickname.getSkin() != null && !nickname.getSkin().isBlank()) ? skinManager.getFromMojang(nickname.getSkin()) : null;
+            
+            Bukkit.getScheduler().runTask(plugin, () -> {
+                if (skin != null && nickname.isActive()) {
+                    plugin.getNickWrapper().setNick(plugin, player, nickname.getName(), skin);
+                }
+                
+                NexusScoreboard scoreboard = new SpigotNexusScoreboard(nexusPlayer);
+                scoreboard.init();
+                
+                nexusPlayer.setScoreboard(scoreboard);
+                
+                NexusPlayerLoadEvent loadEvent = new NexusPlayerLoadEvent(nexusPlayer);
+                try {
+                    Bukkit.getServer().getPluginManager().callEvent(loadEvent);
+                } catch (Exception ex) {
+                    plugin.getLogger().severe("There was an error handling the NexusPlayerLoadEvent. This was caught to prevent other issues. Stack Trace Below");
+                    ex.printStackTrace();
+                }
+                
+                ScoreboardView scoreboardView = loadEvent.getScoreboardView();
+                if (scoreboardView != null) {
+                    scoreboard.setView(scoreboardView);
+                }
+                
+                if (scoreboard.getTablistHandler() == null) {
+                    if (loadEvent.getTablistHandler() != null) {
+                        scoreboard.setTablistHandler(loadEvent.getTablistHandler());
+                    } else {
+                        scoreboard.setTablistHandler(new RankTablistHandler(scoreboard));
+                    }
+                }
+                
+                scoreboard.apply();
+                
+                String joinMessage = loadEvent.getJoinMessage();
+                if (joinMessage != null && !joinMessage.isEmpty()) {
+                    Bukkit.broadcastMessage(StarColors.color(joinMessage));
+                }
+                
+                ActionBar actionBar = new ActionBar();
+                actionBar.send(player, "&aYour data has been loaded!");
+                
+                plugin.getNexusServer().join(nexusPlayer);
+                
+                if (loadEvent.getActionBar() != null) {
+                    new BukkitRunnable() {
+                        public void run() {
+                            nexusPlayer.setActionBar(loadEvent.getActionBar());
+                        }
+                    }.runTaskLater(plugin, 60L);
+                }
+            });
+        });
     }
 
     @EventHandler
@@ -221,37 +235,25 @@ public class SpigotPlayerManager extends PlayerManager implements Listener {
                 session.end();
                 nexusPlayer.setLastLogout(session.getEnd());
                 long playTime = session.getTimeOnline();
-                SQLDatabase database = NexusAPI.getApi().getPrimaryDatabase();
-                Table table = database.getTable(GameInfo.class);
-                String query = "select * from " + table.getName() + " where `gameStart`>='" + session.getStart() + "' and `gameEnd` <= '" + session.getEnd() + "' and `players` like '%" + e.getPlayer().getName() + "%';";
-                try {
-                    List<Row> rows = database.executeQuery(query);
-                    session.setGamesPlayed(rows.size());
-                } catch (SQLException ex) {
-                    NexusAPI.getApi().getLogger().info(query);
-                    ex.printStackTrace();
-                }
-
-                if (session.getGamesPlayed() > 0 || session.getTimeOnline() >= TimeUnit.MINUTES.toMillis(1)) {
-                    database.saveSilent(session);
-                }
+                NexusAPI.getApi().getPrimaryDatabase().deleteSilent(session);
                 nexusPlayer.setSession(null);
                 nexusPlayer.getPlayerTime().addPlaytime(playTime);
                 NexusAPI.getApi().getPrimaryDatabase().saveSilent(nexusPlayer);
             });
-            this.players.remove(nexusPlayer.getUniqueId());
-            this.plugin.getNexusServer().quit(nexusPlayer);
-            if (nexusPlayer.getRank().ordinal() <= Rank.MEDIA.ordinal()) {
-                if (!nexusPlayer.isNicked()) { //TODO Need a change from StarChat to filter receivers
-                    plugin.getStaffChannel().sendMessage(new ChatContext(nexusPlayer.getTrueDisplayName() + " &7disconnected"));
-                }
+        }
+        
+        this.players.remove(nexusPlayer.getUniqueId());
+        this.plugin.getNexusServer().quit(nexusPlayer);
+        if (nexusPlayer.getRank().ordinal() <= Rank.MEDIA.ordinal()) {
+            if (!nexusPlayer.isNicked()) { //TODO Need a change from StarChat to filter receivers
+                plugin.getStaffChannel().sendMessage(new ChatContext(nexusPlayer.getTrueDisplayName() + " &7disconnected"));
             }
         }
     }
 
     @EventHandler
     public void onCommandPreProcess(PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().toLowerCase().startsWith("me")) {
+        if (e.getMessage().toLowerCase().startsWith("minecraft:me")) {
             e.setCancelled(true);
         } else if (e.getMessage().toLowerCase().startsWith("minecraft:tell") || e.getMessage().toLowerCase().startsWith("minecraft:whisper")) {
             e.setCancelled(true);
